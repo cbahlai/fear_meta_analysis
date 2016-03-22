1804a7826cd789318919b75347a651bfe9a3286b
cbahlai <cbahlai@msu.edu>
2016-03-22 12:49:03 -0400

    add fsn, plots

--- test_analysis.R
+++ test_analysis.R
@@ -1,5 +1,10 @@
 # Test meta analysis of subset of data to demonstrate usage of the metafor package
-# using subset of data. Prior to improtation, data was manipulated in excel to remove
+# using subset of data. 
+# analysis based on tuturial located at 
+# http://rstudio-pubs-static.s3.amazonaws.com/10913_5858762ec84b458d89b0f4a4e6dd5e81.html
+
+
+# Prior to importation, data was manipulated in excel to remove
 # embedded metadata and to remove characters R will interpret as operators(+/-) and 
 # replace them with text strings (sign: Neg Pos)
 # metadata is as follows:
@@ -21,6 +26,11 @@ library(metafor)
 #call the citation so it's handy in the output
 citation("metafor")
 
+#also call plyr, we'll need it later
+library(plyr)
+#call the citation so it's handy in the output
+citation("plyr")
+
 #compute effect size
 #use Hedge's g to account for small sample bias
 #Hedge's g is the stadardized mean difference, corrected for negative bias and is encoded as SMD in metafor
@@ -37,6 +47,11 @@ data1<-escalc(n1i=Nc,n2i=Ne,
 
 ma_model_1<-rma(yi, vi, data=data1)
 summary(ma_model_1)
+#note output is based on zval, not t value by default as this is the standard, so this may cause slight 
+#deviations from the Metawin output
+
+#compute failsafe number
+fsn(yi, vi,  data=data1, type="Rosenthal", alpha=.05)
 
 #create forest plot
 forest(ma_model_1, slab=paste(data1$Source, data1$Genus, data1$species))
@@ -44,5 +59,21 @@ forest(ma_model_1, slab=paste(data1$Source, data1$Genus, data1$species))
 #quick test for publication bias- look for asymetrical distribturion
 funnel(ma_model_1)
 
-#does effect strength vary by habitat?
+#does effect size vary by habitat?
 boxplot(yi ~ Habitat, data = data1)
+#summary data
+ddply(data1, c("Habitat"), summarise,
+      mean=mean(yi), sd=mean(vi))
+
+#does effect size vary by type of interaction?
+boxplot(yi ~ Sign, data = data1)
+#summary data
+ddply(data1, c("Sign"), summarise,
+      mean=mean(yi), sd=mean(vi))
+
+#rerun model with these factors accounted for
+ma_model_2<-rma(yi, vi, mods=~Habitat+Sign, data=data1, method="ML")
+summary(ma_model_2)
+
+
+forest(ma_model_2, slab=paste(data1$Source, data1$Genus, data1$species))
